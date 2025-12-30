f595d359ff351414dc55101f4e431969988ae79f
Libby Burrough <lburrough99@yahoo.co.uk>
2025-12-30 23:54:25 +0000

    updated coming soon page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Source_Sans_3 } from "next/font/google";
+
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const sourceSans = Source_Sans_3({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["300", "400", "600", "700"],
+  variable: "--font-source-sans",
 });
 
 export const metadata: Metadata = {
@@ -25,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,18 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen bg-zinc-50 font-sans bg-stone-50">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-stone-50 sm:items-start">
+    <div className="flex min-h-screen bg-zinc-50 bg-[#fbfcf7]">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-[#fbfcf7] sm:items-start">
         <Image
           src="/logo.png"
           alt="Next.js logo"
-          width={500}
-          height={500}
+          width={600}
+          height={600}
           priority
         />
         <div className="flex flex-col items-center gap-6">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black">
-            Coming Soon
+          <h1 className="${sourceSans.variable} text-[#960404] text-3xl">
+            COMING SOON
           </h1>
         </div>
       </main>
